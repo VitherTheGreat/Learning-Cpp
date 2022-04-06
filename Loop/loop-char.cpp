b81ab2e6515037657ee13659293a6c3bce6d363d
VitherTheGreat <102701096+VitherTheGreat@users.noreply.github.com>
2022-04-06 14:32:35 -0700

    Update loop-char.cpp

--- Loop/loop-char.cpp
+++ Loop/loop-char.cpp
@@ -5,13 +5,21 @@ using namespace std;
 // Memasukkan kalimat dan mengulai berapa kali
 
 int main() {
+	int l; // Loop
+	string t; // Text input
 	
 	string a;
 	getline (cin, a);
+	cout << "Enter the text..." << endl ;
+	getline (cin, t);
 	
 	cout << "........." << endl;
+	cout << "How many times you want to repeat it... " ;
+	cin >> l ;
 	
 	for (int i = 1; i <= 5; ++i) {
         cout << a << endl;
+	for (int i = 1 ; i <= l; ++i) {
+        cout << t << endl;
     }
 }
